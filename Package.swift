07038a877dd0cade89c63727a02205634970631a
Swizzlr <git@swizzlr.co>
2017-01-25 18:51:45 -0500

    Use the version of Titan that ships TitanCORS.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     ],
     dependencies: [
       .Package(url: "https://github.com/bermudadigitalstudio/Titan.git", majorVersion: 0, minor: 6),
-      .Package(url: "https://github.com/bermudadigitalstudio/TitanCORS.git", majorVersion: 0, minor: 1),
       .Package(url: "https://github.com/bermudadigitalstudio/TitanNestAdapter.git", majorVersion: 0, minor: 2),
       .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 6),
     ]
